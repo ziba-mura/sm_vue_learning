23c35fbce21bacd0923914d3dbc2ab0045c1a530
ziba-mura <ziba-mura@users.noreply.github.com>
2025-03-16 09:08:42 +0900

    基本的な枠組み作成

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,25 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="wrap">
+    <header>ヘッダーエリア</header>
+    <main class="main">メインエリア</main>
+    <footer>フッターエリア</footer>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.wrap {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 90vh;
+  padding: 0;
+  margin: 0;
+  font-family: Arial, sans-serif;
+  background-color: #f8f8f8;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.main {
+  flex: 1;
+  pad: 16px;
 }
 </style>
