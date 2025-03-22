c2c523166f724efb0c961379cc07e57c2f768b2f
ziba-mura <ziba-mura@users.noreply.github.com>
2025-03-23 08:19:55 +0900

    データのバインディングとイベントハンドリングを追加

--- src/components/MainTodo.vue
+++ src/components/MainTodo.vue
@@ -1,8 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+const todo = ref('')
+const addTodo = () => {
+  localStorage.todoList = todo.value
+}
+</script>
 <template>
   <div>
-    <input type="text" class="todo_input" placeholder="+ TODOを入力" />
-    <button class="btn">追加</button>
+    <input type="text" class="todo_input" v-model="todo" placeholder="+ TODOを入力" />
+    <button class="btn" @click="addTodo">追加</button>
   </div>
 </template>
 
