471156433745e70ca11bfbbfad6ba2017c58d5af
ziba-mura <ziba-mura@users.noreply.github.com>
2025-03-23 08:37:43 +0900

    TODOの一覧表示機能を追加

--- src/components/MainTodo.vue
+++ src/components/MainTodo.vue
@@ -15,6 +15,18 @@ const addTodo = () => {
     <input type="text" class="todo_input" v-model="todo" placeholder="+ TODOを入力" />
     <button class="btn" @click="addTodo">追加</button>
   </div>
+  <div class="box_list">
+    <div class="todo_list" v-for="todo in todoList" :key="todo.id">
+      <div class="todo">
+        <input type="checkbox" class="check" />
+        <label>{{ todo.task }}</label>
+      </div>
+      <div class="btns">
+        <div class="btn green">編</div>
+        <div class="btn pink">削</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
@@ -38,5 +50,36 @@ const addTodo = () => {
 }
 .btn:active {
   top: 1px;
+}
+.box_list {
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  margin-top: 20px;
+}
+.todo_list {
+  display: flex;
+  gap: 8px;
+  align-items: center;
+}
+.todo {
+  width: 250px;
+  padding: 6px 8px;
+  border: 1px solid #ccc;
+  border-radius: 6px;
+}
+.check {
+  margin-right: 12px;
+  transform: scale(1.6);
+}
+.btns {
+  display: flex;
+  gap: 4px;
+}
+.green {
+  background-color: #00c853;
+}
+.pink {
+  background-color: #ff4081;
 }
 </style>
