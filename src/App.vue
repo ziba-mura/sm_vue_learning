db674923a2fb514c8cd003ade9445c6303780414
ziba-mura <ziba-mura@users.noreply.github.com>
2025-03-23 07:56:47 +0900

    TODO追加ボタンコンポーネントを追加

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
+import MainTodo from '@/components/MainTodo.vue'
 import TheHeader from '@/components/TheHeader.vue'
 import TheFooter from '@/components/TheFooter.vue'
 </script>
 <template>
   <div class="wrap">
     <TheHeader />
-    <main class="main">メインエリア</main>
+    <main class="main"><MainTodo /></main>
     <TheFooter />
   </div>
 </template>
---
+++ src/components/MainTodo.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <input type="text" class="todo_input" placeholder="+ TODOを入力" />
+    <button class="btn">追加</button>
+  </div>
+</template>
+
+<style scoped>
+.todo_input {
+  width: 250px;
+  padding: 6px 8px;
+  margin-right: 8px;
+  font-size: 18px;
+  border: 1px solid #aaa;
+  border-radius: 6px;
+}
+.btn {
+  position: relative;
+  padding: 6px 8px;
+  font-size: 14px;
+  color: #fff;
+  text-align: center;
+  background-color: #03a9f4;
+  border: 1px solid #eee;
+  border-radius: 6px;
+}
+.btn:active {
+  top: 1px;
+}
+</style>
